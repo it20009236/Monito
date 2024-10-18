e6bb688563fca5c0aa801902b23af842a0fdc411
IT20009236 <IT20009236@my.sliit.lk>
2024-10-19 00:34:04 +0530

    add responsiveness

---
+++ src/components/ProductDeatail/Cardlist.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+function CustomerGallery() {
+  return (
+    <div className="w-[1180px] mx-auto bg-[#fdfdfd] py-6 rounded-[20px]">
+      <h2 className="text-[24px] font-bold text-[#00171f] pl-4">Our lovely customer</h2>
+      <div className="flex gap-3 pl-4 mt-6 overflow-x-scroll">
+        <img src="./assets/frame-118.svg" alt="Customer 1" className="w-[248px] h-[340px] rounded-[10px]" />
+        <img src="./assets/frame-119.svg" alt="Customer 2" className="w-[248px] h-[340px] rounded-[10px]" />
+        <img src="./assets/frame-120.svg" alt="Customer 3" className="w-[248px] h-[340px] rounded-[10px]" />
+        <img src="./assets/frame-121.svg" alt="Customer 4" className="w-[248px] h-[340px] rounded-[10px]" />
+        <img src="./assets/frame-122.svg" alt="Customer 5" className="w-[248px] h-[340px] rounded-[10px]" />
+      </div>
+      <div className="flex justify-center gap-1.5 mt-6">
+        <img src="./assets/frame-123.svg" alt="Dot 1" className="w-[9px] h-[9px]" />
+        <img src="./assets/frame-124.svg" alt="Dot 2" className="w-[9px] h-[9px]" />
+        <img src="./assets/frame-125.svg" alt="Dot 3" className="w-[9px] h-[9px]" />
+        <img src="./assets/frame-126.svg" alt="Dot 4" className="w-[9px] h-[9px]" />
+        <img src="./assets/frame-127.svg" alt="Dot 5" className="w-[9px] h-[9px]" />
+        <img src="./assets/frame-128.svg" alt="Dot 6" className="w-[9px] h-[9px]" />
+      </div>
+    </div>
+  );
+}
+
+export default CustomerGallery;
